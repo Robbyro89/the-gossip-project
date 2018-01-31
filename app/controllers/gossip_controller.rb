0c25a0201d955301c94ff232b53a596cedf8c356
Robbyro89 <gallery.laroche.id@gmail.com>
2018-01-31 18:33:55 +0800

    generate application Gossip

---
+++ app/controllers/gossip_controller.rb
@@ -0,0 +1,20 @@
+class GossipController < ApplicationController
+  
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'gossip/create'
+
+  get 'gossip/show'
+
+  get 'gossip/edit'
+
+  get 'gossip/index'
+
+  get '/new', to: 'gossip#new'
+
   root 'application#hello'
 end
